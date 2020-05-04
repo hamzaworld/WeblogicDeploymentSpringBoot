1852426611a7607a4a4e31d6c8576d608a83e931
Akhtar, Hamza <hamza.akhtar@spglobal.com>
2020-05-04 20:57:30 +0530

    src test case

---
+++ src/test/java/com/platts/prp/pricereceiver/WeblogicSpringBootApplicationTests.java
@@ -0,0 +1,11 @@
+package com.platts.prp.pricereceiver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WeblogicSpringBootApplicationTests {
+
+	@Test 
+	public void contextLoads() { }
+}
